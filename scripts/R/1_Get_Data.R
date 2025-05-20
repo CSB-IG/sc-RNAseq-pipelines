f63e4f93ecb6fbd26317faca06cf83f3a72126f5
JoelRuiz26 <josejoelruizhernandez@gmail.com>
2025-05-20 18:11:14 +0000

    solved_paths

--- scripts/R/1_Get_Data.R
+++ scripts/R/1_Get_Data.R
@@ -1,10 +1,7 @@
 #Code for download data
-
-dir.create("~/1_Get_Data", showWarnings = FALSE)
-setwd("~/1_Get_Data/")
+dir.create("1_Get_Data", showWarnings = FALSE)
 dir.create("post", showWarnings = FALSE)
 dir.create("pre", showWarnings = FALSE)
-
 
 # Get pre Data
 download.file("https://cf.10xgenomics.com/samples/cell-exp/1.1.0/aml035_pre_transplant/aml035_pre_transplant_fastqs.tar",
@@ -12,12 +9,9 @@ download.file("https://cf.10xgenomics.com/samples/cell-exp/1.1.0/aml035_pre_tran
 # Untar
 untar("pre/aml035_pre_transplant_fastqs.tar", exdir = "pre")
 
-
-
 # Get post data
 download.file("https://cf.10xgenomics.com/samples/cell-exp/1.1.0/aml035_post_transplant/aml035_post_transplant_fastqs.tar",
               destfile = "post/aml035_post_transplant_fastqs.tar", method = "wget")
+
 # untar
 untar("post/aml035_post_transplant_fastqs.tar", exdir = "post")
-
-
